df13e125e37e1ed8b9dbab05fa9d628faa2400a4
Jair Gonzalez <jairgonzalez@gmail.com>
2020-12-03 00:46:30 -0800

    slander

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import react from "react";
 
 export default function Home() {
   return (
-   <div>
-     Eyad is Hella gay
-   </div>
+    
+    <center>
+      <br />
+      <br />
+      <div>
+        <div>
+        "IM EYAD THE BIG GUY"
+        </div>
+      <img src="eyad.jpg" width="214" height="320" />
+      </div>
+    </center>
   )
 }
